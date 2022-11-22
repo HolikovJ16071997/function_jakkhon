9010eb936597debc105b887895a2fdde553011d3
Jony <johongirxolikov@gmail.com>
2022-11-22 10:34:35 +0500

    new js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+for (let i = -10; i < 10; i++) { 
+    console.log(i);
+  }
+
+for (let b =0; b < 100; b+=3) { 
+    console.log(b);
+  }
+
+  for (let a =0; a < 10; a++) { 
+    console.log(a);
+  }
+
+
+
+  function onClik(git) {
+    return  git= "Hello World!";
+  }
+
+  document.querySelector(".demo").innerHTML = onClik('jonu');
+  
